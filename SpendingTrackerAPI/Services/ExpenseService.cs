6105ffc2cfb1bc3964a64491fdee94521e05dcd1
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-05 04:42:49 +0200

    Add ExpenseService

---
+++ SpendingTrackerAPI/Services/ExpenseService.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SpendingTrackerAPI.Database;
+using SpendingTrackerAPI.Entities;
+
+namespace SpendingTrackerAPI.Services;
+
+public interface IExpenseService
+{
+    IEnumerable<Expense> GetAll();
+    Expense GetById(int id);
+    void Create(Expense expense);
+    void Remove(int id);
+    void Update(int id, Expense expense);
+}
+
+public class ExpenseService : IExpenseService
+{
+    private readonly AppDbContext _context;
+
+    public ExpenseService(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    
+    public IEnumerable<Expense> GetAll()
+    {
+        IQueryable<Expense> expenseQuery = _context.Expenses.Include(x => x.ExpenseCategory);
+
+        var expenses = expenseQuery.ToList();
+        return expenses;
+    }
+    
+    public Expense GetById(int id)
+    {
+        var expense = _context.Expenses.Include(x=>x.ExpenseCategory).FirstOrDefault(x => x.Id == id);
+        return expense;
+    }
+    
+    public void Create(Expense expense)
+    {
+        var newExpense = new Expense()
+        {
+            Name = expense.Name,
+            Amount = expense.Amount,
+            ExpenseCategoryId = expense.ExpenseCategoryId
+        };
+        _context.Expenses.Add(newExpense);
+        _context.SaveChanges();
+    }
+    
+    public void Remove(int id)
+    {
+        var expense = _context.Expenses.FirstOrDefault(x => x.Id == id);
+        _context.Expenses.Remove(expense);
+        _context.SaveChanges();
+    }
+    
+    public void Update(int id, Expense expense)
+    {
+        var expenseDB = _context.Expenses.FirstOrDefault(x => x.Id == id);
+        expenseDB.Name = expense.Name;
+        expenseDB.Amount = expense.Amount;
+        expenseDB.ExpenseCategoryId = expense.ExpenseCategoryId;
+        _context.SaveChanges();
+    }
+}
