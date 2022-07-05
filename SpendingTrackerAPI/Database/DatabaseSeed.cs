9c86291f1ce6bdbe0c9a37ea2aa69d2b020c227a
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-05 03:11:13 +0200

    Add seeder

---
+++ SpendingTrackerAPI/Database/DatabaseSeed.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingTrackerAPI.Entities;
+
+namespace SpendingTrackerAPI.Database;
+
+public static class DatabaseSeed
+{
+    public static void Seed(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+        }
+
+        void SeedData(AppDbContext context)
+        {
+            context.Database.Migrate();
+            if (!context.ExpenseCategories.Any())
+            {
+                context.ExpenseCategories.AddRange(
+                    new ExpenseCategory()
+                    {
+                        Name = "Flat"
+                    },
+                    new ExpenseCategory()
+                    {
+                        Name = "Car"
+                    }
+                );
+                context.SaveChanges();
+            }
+
+            if (!context.IncomeCategories.Any())
+            {
+                context.IncomeCategories.AddRange(
+                new IncomeCategory()
+                {
+                    Name= "Work"   
+                },
+                new IncomeCategory()
+                {
+                    Name = "Renting"
+                }
+                );
+            }
+        }
+    }
+    
+}
