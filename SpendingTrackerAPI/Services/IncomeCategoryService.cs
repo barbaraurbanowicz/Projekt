0065fadc8c3922c9a0ad04dae4c7c7b26b375fb0
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-05 04:52:40 +0200

    Add IncomeCategoryService

---
+++ SpendingTrackerAPI/Services/IncomeCategoryService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingTrackerAPI.Database;
+using SpendingTrackerAPI.Entities;
+
+namespace SpendingTrackerAPI.Services;
+
+public interface IIncomeCategoryService
+{
+    IncomeCategory GetById(int id);
+    IEnumerable<IncomeCategory> GetAll();
+    void Create(IncomeCategory incomeCategory);
+    void Update(int id ,IncomeCategory incomeCategory);
+    void Remove(int id);
+}
+
+public class IncomeCategoryService : IIncomeCategoryService
+{
+    private readonly AppDbContext _context;
+
+    public IncomeCategoryService(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public IncomeCategory GetById(int id)
+    {
+        var category = _context.IncomeCategories.FirstOrDefault(x => x.Id == id);
+        return category;
+    }
+    
+    public IEnumerable<IncomeCategory> GetAll()
+    {
+        var categories = _context.IncomeCategories.ToList();
+        return categories;
+    }
+    
+    public void Create(IncomeCategory incomeCategory)
+    {
+        var newCategory = new IncomeCategory()
+        {
+            Name = incomeCategory.Name,
+        };
+
+        _context.IncomeCategories.Add(newCategory);
+        _context.SaveChanges();
+    }
+        
+    public void Update(int id ,IncomeCategory incomeCategory)
+    {
+        var dbCategory = _context.IncomeCategories.FirstOrDefault(c => c.Id == id);
+        dbCategory.Name = incomeCategory.Name;
+        _context.SaveChanges();
+    }
+    
+    public void Remove(int id)
+    {
+        var category = _context.IncomeCategories.FirstOrDefault(c => c.Id == id);
+        _context.IncomeCategories.Remove(category);
+        _context.SaveChanges();
+    }
+}
