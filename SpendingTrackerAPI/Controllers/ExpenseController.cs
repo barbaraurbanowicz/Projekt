27b5fd036acc26e5af59e169a6e91d43bc00bf06
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-05 05:09:53 +0200

    Add ExpenseController

---
+++ SpendingTrackerAPI/Controllers/ExpenseController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpendingTrackerAPI.Entities;
+using SpendingTrackerAPI.Services;
+
+namespace SpendingTrackerAPI.Controllers;
+
+[Route("api/expenses")]
+[ApiController]
+
+public class ExpenseController : Controller
+{
+    private readonly IExpenseService _service;
+
+    public ExpenseController(IExpenseService service)
+    {
+        _service = service;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var expenses = _service.GetAll();
+        return Ok(expenses);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var expense = _service.GetById(id);
+        return Ok(expense);
+    }
+    
+    [HttpPost]
+    public IActionResult Create([FromBody] Expense expense)
+    {
+        _service.Create(expense);
+        return Created($"/api/expenses",null);
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult Update([FromRoute]int id, [FromBody] Expense expense)
+    {
+        _service.Update(id, expense);
+        return Ok();
+    }
+    
+    [HttpDelete("{id}")]
+    public ActionResult Delete([FromRoute] int id)
+    {
+        _service.Remove(id);
+        return NoContent();
+    }
+}
