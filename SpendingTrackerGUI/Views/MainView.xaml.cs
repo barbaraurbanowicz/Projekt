13b751144ec29d49af5f9213ea468b68edb93b27
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-06 06:05:27 +0200

    Edit MainView

--- SpendingTrackerGUI/Views/MainView.xaml.cs
+++ SpendingTrackerGUI/Views/MainView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using SpendingTrackerAPI.Entities;
 
 namespace SpendingTrackerGUI.Views;
 
@@ -7,5 +13,49 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        TotalExpenes();
+        TotalIncomes();
+    }
+
+
+    private async void TotalExpenes()
+    {
+        List<Expense> model = null;
+        int sum = 0;
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:5001/api/expenses");
+        response.EnsureSuccessStatusCode();
+        if (response.IsSuccessStatusCode)
+        {
+            string message = await response.Content.ReadAsStringAsync();
+            model = JsonConvert.DeserializeObject<List<Expense>>(message);
+        }
+        
+        foreach(var income in model)
+        {
+            sum += income.Amount;
+        }
+
+        totalExpenses.Text = sum.ToString();
+    }
+    private async void TotalIncomes()
+    {
+        List<Income> model = null;
+        int sum = 0;
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:5001/api/incomes");
+        response.EnsureSuccessStatusCode();
+        if (response.IsSuccessStatusCode)
+        {
+            string message = await response.Content.ReadAsStringAsync();
+            model = JsonConvert.DeserializeObject<List<Income>>(message);
+        }
+        
+        foreach(var income in model)
+        {
+            sum += income.Amount;
+        }
+
+        totalIncomes.Text = sum.ToString();
     }
 }
