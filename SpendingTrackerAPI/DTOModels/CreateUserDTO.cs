051d0c51db06a480fdd9d877393447260bcb6568
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-15 02:53:56 +0200

    Add dtos

---
+++ SpendingTrackerAPI/DTOModels/CreateUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SpendingTrackerAPI.DTOModels;
+
+public class CreateUserDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public int RoleId { get; set; } = 1;
+}
---
+++ SpendingTrackerAPI/DTOModels/LoginUserDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace SpendingTrackerAPI.DTOModels;
+
+public class LoginUserDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
