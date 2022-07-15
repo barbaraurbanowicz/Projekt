11b245eae2d52d11020f783145d40f1880cfb6e7
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-15 02:51:53 +0200

    Add user entities

---
+++ SpendingTrackerAPI/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SpendingTrackerAPI.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public int RoleId { get; set; }
+    public virtual Role Role { get; set; }
+}
