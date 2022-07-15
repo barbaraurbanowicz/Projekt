7b18e1f3fce4de925d696d745a7a4685bea9c76e
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-15 03:18:04 +0200

    Add migration

--- SpendingTrackerAPI/Migrations/20220715011642_Users.Designer.cs
+++ SpendingTrackerAPI/Migrations/20220715011642_Users.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpendingTrackerAPI.Database;
 
@@ -10,9 +11,10 @@ using SpendingTrackerAPI.Database;
 namespace SpendingTrackerAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220715011642_Users")]
+    partial class Users
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
