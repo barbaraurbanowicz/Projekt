74b0cae05839c37094e2fe3d6d8b94a1acc977ca
Barbara Urbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-07-15 03:20:57 +0200

    Add UserController

---
+++ SpendingTrackerAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpendingTrackerAPI.DTOModels;
+using SpendingTrackerAPI.Services;
+
+namespace SpendingTrackerAPI.Controllers;
+
+
+[Route("api/users")]
+[ApiController]
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody]CreateUserDTO createUserDto)
+    {
+        _userService.Register(createUserDto);
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public ActionResult Login([FromBody]LoginUserDTO loginUserDto)
+    {
+        string token = _userService.Login(loginUserDto);
+        return Ok(token);
+    }
+}
